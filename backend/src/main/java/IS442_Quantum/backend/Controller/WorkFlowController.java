624ece94c3927624af7641e4d4861df5e35f24dd
Liao jia xiong <jxliao.2020@scis.smu.edu.sg>
2023-02-19 21:49:27 +0800

    update Workflow
    included formSequence and mapped the mapping of one to many but the API is not yet ready.

--- backend/src/main/java/IS442_Quantum/backend/Controller/WorkFlowController.java
+++ backend/src/main/java/IS442_Quantum/backend/Controller/WorkFlowController.java
@@ -6,6 +6,7 @@ import IS442_Quantum.backend.Repository.WorkFlowRepository;
 import IS442_Quantum.backend.Service.FormService;
 import IS442_Quantum.backend.Service.WorkFlowService;
 import lombok.Data;
+import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,9 +29,30 @@ public class WorkFlowController {
         return new ResponseEntity<>(workFlowService.getAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/workflow/{id}")
+    @GetMapping("/workflows/{id}")
     public Optional<WorkFlow> getForm(@PathVariable Long id){
         return workFlowRepository.findById(id);
+    }
+
+    @PostMapping("/workflows")
+    public ResponseEntity<?> newWorkFlow(@RequestBody WorkFlow newWorkFlow){
+        return new ResponseEntity<>(workFlowService.createWorkFlow(newWorkFlow), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/workflows/{id}")
+    public ResponseEntity<?> deleteWorkFlowById(@PathVariable Long id){
+        if(id == 0 || id == null || !workFlowService.checkWorkFlowById(id)){
+            return new ResponseEntity<>("Delete failed, please provide a valid workflow ID", HttpStatus.BAD_REQUEST);
+        } else {
+            Optional<WorkFlow> tempWorkFlow = workFlowService.getWorkFlowById(id);
+            workFlowService.deleteWorkFlowById(id);
+            return new ResponseEntity<>(tempWorkFlow, HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("/workflows/{id}")
+    public  ResponseEntity<?> updateWorkFlowById(@RequestBody WorkFlow newWorkFlow ,@PathVariable Long id ){
+        return new ResponseEntity<>(workFlowService.createWorkFlow(newWorkFlow), HttpStatus.OK);
     }
 
 
---
+++ backend/src/main/java/IS442_Quantum/backend/Repository/FormSequenceRepository.java
@@ -0,0 +1,7 @@
+package IS442_Quantum.backend.Repository;
+
+import IS442_Quantum.backend.Model.FormSequence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormSequenceRepository extends JpaRepository<FormSequence, Long> {
+}
