b2edf0525574a2d64c5226de69de34d1b083e202
absolutelynoot <isalsamudra@hotmail.com>
2023-02-24 14:03:28 +0800

    Added CORS backend

--- backend/src/main/java/IS442_Quantum/backend/Controller/QuestionController.java
+++ backend/src/main/java/IS442_Quantum/backend/Controller/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class QuestionController {
 
     private final QuestionService questionService;
--- backend/src/main/java/IS442_Quantum/backend/Controller/UserController.java
+++ backend/src/main/java/IS442_Quantum/backend/Controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
