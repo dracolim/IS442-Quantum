c463e23503c1a4aef7b6cbfa6936f25feba478eb
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2023-02-28 14:47:39 +0800

    removed user service interface

--- backend/src/main/java/IS442_Quantum/backend/Controller/UserController.java
+++ backend/src/main/java/IS442_Quantum/backend/Controller/UserController.java
@@ -4,7 +4,8 @@ import IS442_Quantum.backend.Enums.UserTypes;
 import IS442_Quantum.backend.Model.User;
 import IS442_Quantum.backend.Model.Vendor;
 import IS442_Quantum.backend.Model.WorkFlow;
-import IS442_Quantum.backend.Service.services.UserService;
+import IS442_Quantum.backend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
     private final UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
