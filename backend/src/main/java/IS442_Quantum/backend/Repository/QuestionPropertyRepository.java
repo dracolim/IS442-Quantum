a15c6e3261550149e64d0d1a8b2d497d7278373d
absolutelynoot <isalsamudra@hotmail.com>
2023-03-14 16:44:37 +0800

    Fixed bug workflow table not displaying correct admin and approver

---
+++ backend/src/main/java/IS442_Quantum/backend/Repository/QuestionPropertyRepository.java
@@ -0,0 +1,7 @@
+package IS442_Quantum.backend.Repository;
+
+import IS442_Quantum.backend.Model.QuestionProperty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionPropertyRepository extends JpaRepository<QuestionProperty, Long> {
+}
